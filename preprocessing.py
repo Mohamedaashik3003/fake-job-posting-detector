aa4fa84133c96c0dbca734a0a60b54f916430f36
Mohamedaashik3003 <aaashikmohamed859@gmail.com>
2025-12-23 09:58:00 +0530

    Day 2: Text preprocessing using local dataset

--- preprocessing.py
+++ preprocessing.py
@@ -1,1 +1,18 @@
-# Text preprocessing logic will be implemented here
+import pandas as pd
+import re
+import nltk
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+df = pd.read_csv(
+    r"C:/Users/aashi/OneDrive/Desktop/fake job detector/fake-job-posting-detector-main/data/fake_job_postings.csv"
+)
+stop_words = set(stopwords.words('english'))
+def clean_text(text):
+    text = str(text).lower()
+    text = re.sub(r'[^a-z\s]', '', text)
+    text = ' '.join(word for word in text.split() if word not in stop_words)
+    return text
+df['clean_text'] = df['description'].apply(clean_text)
+df.to_csv( r"C:/Users/aashi/OneDrive/Desktop/fake job detector/fake-job-posting-detector-main/data/cleaned_data.csv", index=False)
+
+
